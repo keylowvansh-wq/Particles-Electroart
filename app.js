a13b161f67e4c2a58195f65d93dd275e9def74a7
keylowvansh-wq <keylowvansh@gmail.com>
2026-08-19 19:21:21 +0530

    Added flow field math and physics, color palletes

--- app.js
+++ app.js
@@ -10,6 +10,23 @@ c.fillRect(0, 0, w, h);
 }
 window.addEventListener('resize', handleResize);
 handleResize();
+var palettes =[
+{
+h: 320, s: 80, l: 60 
+},
+{
+h: 190, s: 90, l: 50 
+},
+{
+h: 40, s: 90, l: 60
+},
+{
+h: 280, s: 70, l: 55 
+},
+{
+h: 120, s: 60, l: 45 
+}
+];
 class P{
 constructor(){
 this.x = Math.random() * w;
@@ -19,11 +36,30 @@ this.vy = 0;
 this.life = 0;
 this.maxLife = 100 + Math.random() * 150;
 this.size = Math.random() * 1.5 + 0.5;
+this.palIdx = Math.floor(Math.random() * palettes.length);
 }
-update(){
+update(time, zoom){
+var nx = this.x * zoom;
+var ny = this.y * zoom;
+var n1 = Math.sin(nx + time);
+var n2 = Math.cos(ny - time);
+var n3 = Math.sin((nx + ny) * 0.5 + time * 0.5);
+var angle = (n1 * n2 + n3) * Math.PI * 4;
+this.vx += Math.cos(angle) * 0.15;
+this.vy += Math.sin(angle) * 0.15;
+var speed = Math.sqrt(this.vx * this.vx + this.vy * this.vy);
+var maxSpeed = 4;
+if(speed > maxSpeed){
+this.vx = (this.vx / speed) * maxSpeed;
+this.vy = (this.vy / speed) * maxSpeed;
+}
+
+this.vx *= 0.98;
+this.vy *= 0.98;
 this.x += this.vx;
 this.y += this.vy;
 this.life++;
+
 if(this.x < 0)
 this.x = w;
 if(this.x > w)
@@ -32,35 +68,41 @@ if(this.y < 0)
 this.y = h;
 if(this.y > h)
 this.y = 0;
+
 if(this.life > this.maxLife){
 this.x = Math.random() * w;
 this.y = Math.random() * h;
 this.vx = 0;
 this.vy = 0;
 this.life = 0;
+this.palIdx = Math.floor(Math.random() * palettes.length);
  }
 }
 draw(){
-var alpha = Math.sin((this.life / this.maxLife) * Math.PI);
-c.fillStyle = 'rgba(255, 255, 255, ' + alpha + ')';
+var lifeRatio = this.life / this.maxLife;
+var alpha = Math.sin(lifeRatio * Math.PI);
+var p = palettes[this.palIdx];
+var hueShift = lifeRatio * 40;
+var h = (p.h + hueShift) % 360;
+var s = p.s;
+var l = p.l;
+c.fillStyle = 'hsla(' + h + ', ' + s + '%, ' + l + '%, ' + alpha + ')';
 c.fillRect(this.x, this.y, this.size, this.size);
  }
 }
 for(var i = 0; i < 1500; i++){
 pts.push(new P());
 }
-
 var t = 0;
-
+var z = 0.005;
 function loop(){
 c.fillStyle = 'rgba(5, 5, 5, 0.05)';
 c.fillRect(0, 0, w, h);
-
 for(var i = 0; i < pts.length; i++){
-pts[i].update();
+pts[i].update(t, z);
 pts[i].draw();
 }
-t += 0.01;
+t += 0.005;
 requestAnimationFrame(loop);
 }
 loop();
